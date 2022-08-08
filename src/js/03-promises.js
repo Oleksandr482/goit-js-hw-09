923c70ba6449c474ae7e05a660dc776aacefecd1
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-08-08 22:25:23 +0300

    task 3 is finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+}
+
+refs.form.addEventListener('submit', onFoprmSubmit)
+
+function onFoprmSubmit(e) {
+  e.preventDefault();
+  let delay = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    let position = i;
+    createPromise(position, delay).then(Notify.success).catch(Notify.failure);
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }  
+  }, delay)
+  })
 }
